6daa3270d0f318906fca4af0d118fc04e326748c
yumauda <firststep-yumauda@gmail.com>
2025-09-06 17:43:41 +0900

    最初のコミット

---
+++ front-page.php
@@ -0,0 +1,84 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-top-works">
+    <div class="l-inner">
+      <div class="p-top-works__content">
+        <div class="p-top-works__title-wrapper">
+          <h2 class="p-top-works__title">
+            <span class="p-top-works__title-en">Works</span>
+            <span class="p-top-works__title-ja">施工実績</span>
+          </h2>
+        </div>
+        <div class="p-top-works__slider-wrapper">
+          <div class="p-top-works__slider">
+            <!-- Slider main container -->
+            <div class="swiper swiper1">
+              <!-- Additional required wrapper -->
+              <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+              </div>
+              <!-- If we need pagination -->
+              <div class="swiper-pagination"></div>
+
+              <!-- If we need navigation buttons -->
+              <div class="swiper-button-prev"></div>
+              <div class="swiper-button-next"></div>
+
+              <!-- If we need scrollbar -->
+              <div class="swiper-scrollbar"></div>
+            </div>
+          </div>
+          <div class="p-top-works__slider">
+            <!-- Slider main container -->
+            <div class="swiper swiper1">
+              <!-- Additional required wrapper -->
+              <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+                <div class="swiper-slide">
+                  <figure class="p-top-works__img">
+                    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/slider_1.jpg" alt="" width="420" height="280">
+                  </figure>
+                </div>
+              </div>
+              <!-- If we need pagination -->
+              <div class="swiper-pagination"></div>
+
+              <!-- If we need navigation buttons -->
+              <div class="swiper-button-prev"></div>
+              <div class="swiper-button-next"></div>
+
+              <!-- If we need scrollbar -->
+              <div class="swiper-scrollbar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer() ?>
