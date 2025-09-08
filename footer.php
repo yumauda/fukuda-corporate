26be31627340bfa998e85e1ed22aeb09609eb117
yumauda <firststep-yumauda@gmail.com>
2025-09-08 14:15:20 +0900

    footer部分構築

--- footer.php
+++ footer.php
@@ -1,6 +1,121 @@
-<footer class="l-footer p-footer">
+<div class="p-submit">
     <div class="l-inner">
-        footer.php
+        <a href="#" class="p-submit__content">
+            <div class="p-submit__title-wrapper">
+                <h2 class="p-submit__title">
+                    <span class="p-submit__title-en">contact</span>
+                    <span class="p-submit__title-ja">お問い合わせ</span>
+                </h2>
+            </div>
+            <div class="p-submit__circle">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/btn_icon.png" alt="" width="15" height="12">
+            </div>
+        </a>
+    </div>
+</div>
+
+<footer class="p-footer">
+    <div class="l-inner">
+        <div class="p-footer__content">
+            <div class="p-footer__left">
+                <div class="p-footer__logo">
+                    <a href="#" class="p-footer__home">
+                        <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/footer_logo.png" alt="株式会社フクダ" width="350" height="67">
+                    </a>
+                </div>
+                <div class="p-footer__address-wrapper">
+                    <p class="p-footer__address">〒699-0551 島根県出雲市斐川町沖洲1080番地</p>
+                    <p class="p-footer__address">TEL : <a href="tel:0853720201">0853-72-0201</a> / FAX : 0853-72-0205</p>
+                </div>
+                <ul class="p-footer__icon-lists">
+                    <li class="p-footer__icon-list">
+                        <a href="#" class="p-footer__icon-link">
+                            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/facebook_icon.png" alt="" width="15" height="12">
+                        </a>
+                    </li>
+                    <li class="p-footer__icon-list">
+                        <a href="#" class="p-footer__icon-link p-footer__icon-link--instagram">
+                            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/instagram_icon.png" alt="" width="15" height="12">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="p-footer__right">
+                <ul class="p-footer__lists">
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">事業内容</a>
+                        <ul class="p-footer__sub-lists">
+
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">土木事業</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">建築事業</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">アグリ事業</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">ソーラー発電事業</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">不動産事業</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">施工実績</a>
+                        <ul class="p-footer__sub-lists">
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">土木事業</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">建築事業</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">会社概要</a>
+                        <ul class="p-footer__sub-lists">
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">会社概要</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">認定登録一覧</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">拠点情報</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">新社屋のご案内</a>
+                        <a href="#" class="p-footer__link">私たちの取り組み</a>
+                        <ul class="p-footer__sub-lists">
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">SDGs</a>
+                            </li>
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">フクダ共創サイクル</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-footer__list">
+                        <a href="#" class="p-footer__link">お知らせ</a>
+                        <a href="#" class="p-footer__link">採用情報</a>
+                        <a href="#" class="p-footer__link">お問い合わせ</a>
+                        <ul class="p-footer__sub-lists">
+                            <li class="p-footer__sub-list">
+                                <a href="#" class="p-footer__sub-link">個人情報保護方針</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="p-footer__small">
+            <small>©FUKUDA CONSTRUCTION CO.,LTD</small>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
