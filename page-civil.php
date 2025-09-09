b695a297d66928bcc8e13a7d10b9ff0276c557ce
yumauda <firststep-yumauda@gmail.com>
2025-09-09 11:48:36 +0900

    土木事業ページ構築

---
+++ page-civil.php
@@ -0,0 +1,254 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <div class="p-page-mv__title">
+            <h1 class="c-main-title">
+              <span class="c-main-title__en">Our Business</span>
+              <span class="c-main-title__ja">土木事業</span>
+            </h1>
+          </div>
+          <ul class="p-page-mv__lists">
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">建築事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">アグリ事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">ソーラー発電事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">不動産事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-page-mvImg">
+    <div class="l-inner">
+      <figure class="p-page-mvImg__img">
+        <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_civil.png" alt="土木事業" width="1700" height="500">
+      </figure>
+    </div>
+  </section>
+  <section class="p-intro">
+    <div class="l-inner">
+      <div class="p-intro__content">
+        <div class="p-intro__left">
+          <div class="p-intro__title">
+            <h2 class="c-intro-title">
+              <span class="c-intro-title__en">Civil Engineering</span>
+              <span class="c-intro-title__ja">土木事業</span>
+            </h2>
+          </div>
+          <div class="p-intro__detail">
+            <h3 class="p-intro__detail-title">地域とともに築く、100年の基盤</h3>
+            <p class="p-intro__text">地域社会の基盤を支えるインフラ整備を通じて、安全・安心で持続可能な暮らしの実現に貢献しています。<br>
+              具体的には、道路・橋梁・トンネル・河川・上下水道・鉄道・港湾といった、私たちの生活に欠かせない社会資本の整備・補修・維持管理を手がけています。<br>
+              これらの工事は、自然災害への備えや交通の利便性向上、地域経済の活性化といった多面的な役割を果たしており、長期的な視野に立ったまちづくりの一翼を担っています。</p>
+          </div>
+        </div>
+        <figure class="p-intro__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/intro_civil_img.png" alt="地域とともに築く、100年の基盤" width="630" height="420">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-kind">
+    <div class="l-inner">
+      <div class="p-kind__content">
+        <div class="p-kind__title">
+          <h2 class="c-small-title">
+            施工種別
+          </h2>
+        </div>
+        <ul class="p-kind__lists">
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link p-kind__link--2">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">01</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img1.jpg" alt="道路" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">道路</p>
+                <p class="p-kind__detail-text">新設道路の整備や既存道路の拡幅・改良、舗装の打ち替え、排水設備の設置などを行っています。交通の安全性や快適性を確保するために、計画・設計から施工、維持管理まで一貫して対応します。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">02</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img2.jpg" alt="橋梁" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">橋梁</p>
+                <p class="p-kind__detail-text">新設橋の建設をはじめ、老朽化した橋の補修・補強、耐震補強や長寿命化対策などを行っています。安全性や耐久性、景観との調和にも配慮しながら、高度な技術と精密な施工管理によって橋の機能と信頼性を維持します。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link p-kind__link--3">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">03</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img3.jpg" alt="河川" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">河川</p>
+                <p class="p-kind__detail-text">護岸の整備や堤防の築造・補強、河道の掘削、排水施設の設置などを行っています。洪水や浸水被害を防ぐとともに、水辺環境の保全にも配慮し、安全性と環境性を両立した施工によって河川の機能を維持します。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link p-kind__link--4">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">04</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img4.jpg" alt="河川" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">トンネル</p>
+                <p class="p-kind__detail-text">山間部や都市部における道路・鉄道トンネルの新設、老朽化したトンネルの補修・補強、耐震補強などを行っています。限られた空間での作業や地質条件への対応が求められるため、高度な技術と安全管理により、機能性と安全性を確保します。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link p-kind__link--5">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">05</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img5.jpg" alt="河川" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">鉄道</p>
+                <p class="p-kind__detail-text">線路の敷設や軌道の補修・更新、駅舎や関連施設の整備、橋梁・トンネルなど鉄道インフラの建設・改良を行っています。安全かつ安定した輸送を支えるため、精度の高い施工と厳格な工程管理のもとで、高い品質が求められます。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="p-kind__list">
+            <a href="#" class="p-kind__link p-kind__link--6">
+              <div class="p-number__top-wrapper">
+                <div class="p-kind__number">
+                  <p class="p-kind__number-en">category</p>
+                  <p class="p-kind__number-num">06</p>
+                </div>
+                <figure class="p-kind__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/kind_img6.jpg" alt="湾港" width="240" height="160">
+                </figure>
+              </div>
+              <div class="p-kind__detail">
+                <p class="p-kind__detail-title">湾港</p>
+                <p class="p-kind__detail-text">港湾施設の整備や防波堤の築造、岸壁や埠頭の改良、荷役設備の設置などを行っています。船舶の安全な接岸や物流の効率化を支えるため、海洋環境への配慮と高度な技術を駆使して、安定した運営をサポートします。</p>
+              </div>
+              <div class="p-kind__btn-wrapper">
+                <div class="p-kind__btn">
+                  <p class="p-kind__btn-text">施工実績</p>
+                  <div class="p-kind__btn-icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/kind_icon.png" alt="" width="15" height="12">
+                  </div>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <div class="p-kind__bottom-btnWrapper">
+          <a href="#" class="p-kind__bottom-btn">
+            <p class="p-kind__bottom-btn-text">施工実績を見る</p>
+            <div class="p-kind__bottom-btn-icon">
+              <img src="<?php echo get_template_directory_uri() ?>/images/common/btn_arrow.png" alt="" width="46" height="46">
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer() ?>
