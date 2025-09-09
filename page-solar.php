2eb10abedcbdd3aa40b012a7a9350397b42793ea
yumauda <firststep-yumauda@gmail.com>
2025-09-09 16:05:21 +0900

    ソーラー発電事業ページ

---
+++ page-solar.php
@@ -0,0 +1,162 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <div class="p-page-mv__title">
+            <h1 class="c-main-title">
+              <span class="c-main-title__en">Our Business</span>
+              <span class="c-main-title__ja">ソーラー発電事業</span>
+            </h1>
+          </div>
+          <ul class="p-page-mv__lists">
+            <li class="p-page-mv__list">
+              <a href="<?php echo esc_url(home_url('/civil')); ?>" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">土木事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+            <li class="p-page-mv__list">
+              <a href="<?php echo esc_url(home_url('/architecture')); ?>" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">建築事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">アグリ事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+
+            <li class="p-page-mv__list">
+              <a href="#" class="p-page-mv__link">
+                <p class="p-page-mv__link-text">不動産事業</p>
+                <div class="p-page-mv__icon">
+                  <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon.png" alt="" width="30" height="30">
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="p-page-mvImg">
+    <div class="l-inner">
+      <figure class="p-page-mvImg__img">
+        <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_solar.png" alt="ソーラー発電事業" width="1700" height="500">
+      </figure>
+    </div>
+  </section>
+  <section class="p-intro">
+    <div class="l-inner">
+      <div class="p-intro__content">
+        <div class="p-intro__left">
+          <div class="p-intro__title">
+            <h2 class="c-intro-title">
+              <span class="c-intro-title__en">Solar power generation</span>
+              <span class="c-intro-title__ja">ソーラー発電事業</span>
+            </h2>
+          </div>
+          <div class="p-intro__detail">
+            <h3 class="p-intro__detail-title">土木の知見 × 再生可能エネルギーで、<br>
+              地域に電力という"価値"を生む</h3>
+            <p class="p-intro__text">建設業として長年培ってきた造成・構造物設計・インフラ整備の技術を活かし、再生可能エネルギーの普及に貢献しています。</p>
+          </div>
+          <div class="p-intro__white">
+            <p class="p-intro__white-title">なぜ建設会社がソーラー発電事業に取り組むのか？</p>
+            <div class="p-intro__white-textWrapper">
+              <p class="p-intro__white-text">造成・基礎・構造物施工などの土木技術を活かせる</p>
+              <p class="p-intro__white-text">建設業の枠を超えた「地域インフラ企業」として進化するため</p>
+            </div>
+          </div>
+        </div>
+        <figure class="p-intro__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/intro_solar_img.png" alt="人を想い、空間を創る。" width="630" height="420">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-success">
+    <div class="l-inner">
+      <div class="p-success__content">
+        <div class="p-success__title-wrapper">
+          <h2 class="p-success__title">ソーラーファーム斐川は年間400tものCO₂排出量の削減に成功しています。</h2>
+        </div>
+        <div class="p-success__row">
+          <div class="p-success__block">
+            <div class="p-success__blockTitle-wrapper">
+              <h3 class="p-success__blockTitle">事業経過</h3>
+            </div>
+            <dl class="p-success__dl">
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成24年07月31日</dt>
+                <dd class="p-success__dd">中国電力株式会社へ予備検討申し込み書を提出</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成24年10月03日</dt>
+                <dd class="p-success__dd">中国電力株式会社へ連系検討申込書を提出</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成25年02月13日</dt>
+                <dd class="p-success__dd">中国電力株式会社から連系検討の回答書を受領</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成25年03月07日</dt>
+                <dd class="p-success__dd">経済産業省から設備認定を受領</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成25年06月10日</dt>
+                <dd class="p-success__dd">工事着手</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">平成25年11月19日</dt>
+                <dd class="p-success__dd">竣工式、売電開始</dd>
+              </div>
+            </dl>
+          </div>
+          <div class="p-success__block">
+            <div class="p-success__blockTitle-wrapper">
+              <h3 class="p-success__blockTitle">発電規模</h3>
+            </div>
+            <dl class="p-success__dl">
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">開発面積</dt>
+                <dd class="p-success__dd">13,004.52㎡</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">太陽光発電築造面</dt>
+                <dd class="p-success__dd">6,355.56㎡</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">定格出力</dt>
+                <dd class="p-success__dd">967.68kw</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">設置パネル</dt>
+                <dd class="p-success__dd">パナソニック社製、1枚当り240w<br>
+                  4,032枚（一般家庭約300世帯相当）</dd>
+              </div>
+              <div class="p-success__dlRow">
+                <dt class="p-success__dt">CO2削減量</dt>
+                <dd class="p-success__dd">1年間／400t手</dd>
+              </div>
+
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+<?php get_footer() ?>
