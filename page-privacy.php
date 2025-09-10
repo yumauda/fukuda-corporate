cc291bc5e2d65ed36b59acf350ba5c1cb23c9e79
yumauda <firststep-yumauda@gmail.com>
2025-09-10 16:03:58 +0900

    プライバシーポリシー

---
+++ page-privacy.php
@@ -0,0 +1,127 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <div class="p-page-mv__title">
+            <h1 class="c-main-title">
+              <span class="c-main-title__en">Privacy Policy</span>
+              <span class="c-main-title__ja">個人情報保護方針</span>
+            </h1>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-page-mvImg">
+    <div class="l-inner">
+      <figure class="p-page-mvImg__img">
+        <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_private.png" alt="個人情報保護方針" width="1700" height="500">
+      </figure>
+    </div>
+  </section>
+  <section class="p-privacy">
+    <div class="l-inner">
+      <div class="p-privacy__content">
+        <div class="p-privacy__top">
+          <div class="p-privacy__title">
+            <h2 class="c-small-title">個人情報のお取り扱いについて</h2>
+          </div>
+          <div class="p-privacy__top-textWrapper">
+            <p class="p-privacy__top-text">株式会社フクダ（以下、当社）は、個人情報を適切に保護することが、<br>
+              事業活動を継続していくにあたり当社に課せられた社会的責務であると認識し、<br>
+              以下の方針に基づき個人情報の保護に努めます。</p>
+          </div>
+        </div>
+        <div class="p-privacy__block-wrapper">
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">01</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報の取り扱いについて</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">(1)個人情報の収集<br>
+                  当社は、個人情報を収集する際には、お客様に対し個人情報の利用目的を明確にし、その目的達成に必要な範囲内において適法かつ公正な手段を用います。</p>
+                <p class="p-privacy__text">(2)個人情報の利用<br>
+                  当社は、個人情報を利用する際には、収集時にお客様から同意を得た利用目的の範囲内でのみ利用いたします。</p>
+                <p class="p-privacy__text">(3)個人情報の提供<br>
+                  当社は、個人情報を第三者に提供する際は、事前にお客様の同意を得てから行います。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">02</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報の取り扱いについて</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">(1)個人情報の収集<br>
+                  当社は、個人情報を収集する際には、お客様に対し個人情報の利用目的を明確にし、その目的達成に必要な範囲内において適法かつ公正な手段を用います。</p>
+                <p class="p-privacy__text">(2)個人情報の利用<br>
+                  当社は、個人情報を利用する際には、収集時にお客様から同意を得た利用目的の範囲内でのみ利用いたします。</p>
+                <p class="p-privacy__text">(3)個人情報の提供<br>
+                  当社は、個人情報を第三者に提供する際は、事前にお客様の同意を得てから行います。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">03</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報の開示、訂正等について</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">当社は、お客様からご自身の個人情報の開示、訂正、削除、利用の停止を求められた場合は、合理的な範囲内において速やかに対応いたします。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">04</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報保護の組織・体制について</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">当社は、個人情報管理責任者を任命し、当社従業員ならびにその関係者に対し、個人情報の適正な取扱いを周知徹底いたします。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">05</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報に関する法令・規範の尊守について</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">当社は、適正な個人情報保護活動を維持するため、個人情報に関連する法令その他の規範を尊守いたします。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__block">
+            <div class="p-privacy__number-wrapper">
+              <p class="p-privacy__number">06</p>
+            </div>
+            <div class="p-privacy__detail">
+              <h3 class="p-privacy__block-title">個人情報保護活動の継続的改善について</h3>
+              <div class="p-privacy__text-wrapper">
+                <p class="p-privacy__text">当社は、これらの個人情報保護の取り組みについて、当社が適正な運用を実施しているかを定期的に見直し、継続的改善を図ってまいります。</p>
+              </div>
+            </div>
+          </div>
+          <div class="p-privacy__right">
+            <p class="p-privacy__right-text">令和2年1月17日制定</p>
+            <p class="p-privacy__right-text">株式会社フクダ</p>
+            <p class="p-privacy__right-text">代表取締役社長 福田 弘道</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</main>
+<?php get_footer() ?>
