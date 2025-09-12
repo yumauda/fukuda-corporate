4be2f76ae6f22adacf55547e21f7da71f3b55e07
yumauda <firststep-yumauda@gmail.com>
2025-09-12 15:21:05 +0900

    私たちの取り組みページ

--- page-sustainability.php
+++ page-sustainability.php
@@ -12,7 +12,7 @@
           </div>
           <ul class="p-page-mv__lists">
             <li class="p-page-mv__list">
-              <a href="<?php echo esc_url(home_url('/civil')); ?>" class="p-page-mv__link">
+              <a href="#cycle" class="p-page-mv__link">
                 <p class="p-page-mv__link-text p-page-mv__link-text--white">フクダ共創サイクル</p>
                 <div class="p-page-mv__icon">
                   <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon_white.png" alt="" width="30" height="30">
@@ -20,7 +20,7 @@
               </a>
             </li>
             <li class="p-page-mv__list">
-              <a href="<?php echo esc_url(home_url('/architecture')); ?>" class="p-page-mv__link">
+              <a href="#sdgs" class="p-page-mv__link">
                 <p class="p-page-mv__link-text p-page-mv__link-text--white">SDGs</p>
                 <div class="p-page-mv__icon">
                   <img src="<?php echo get_template_directory_uri() ?>/images/common/page_mv_icon_white.png" alt="" width="30" height="30">
@@ -40,7 +40,7 @@
       </figure>
     </div>
   </section>
-  <section class="p-cycle">
+  <section class="p-cycle" id="cycle">
     <div class="l-inner">
       <div class="p-cycle__content">
         <div class="p-cycle__title">
@@ -55,6 +55,107 @@
         <figure class="p-cycle__img">
           <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/cycle_img.png" alt=""フクダ"と関わる全ての人と、喜び合える関係でありたい！" width="1400" height="1400">
         </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-sdgs" id="sdgs">
+    <div class="l-inner">
+      <div class="p-sdgs__content">
+        <div class="p-sdgs__title">
+          <h2 class="c-intro-title">
+            <span class="c-intro-title__en c-intro-title__en--white">Co-creation cycle</span>
+            <span class="c-intro-title__ja c-intro-title__ja--white">フクダ共創サイクル</span>
+          </h2>
+        </div>
+        <div class="p-sdgs__block-wrapper">
+          <div class="p-sdgs__block">
+            <div class="p-sdgs__detail">
+              <p class="p-sdgs__detail-subTitle">インフラ整備</p>
+              <h3 class="p-sdgs__detail-title">高い質のインフラと住まいの建設</h3>
+              <p class="p-sdgs__text">快適で安心安全なインフラ・住まいを求めるお客様の喜び・満足の追及を通して、高い品質の工事に努め、社会に貢献します。</p>
+              <div class="p-sdgs__white">
+                <p class="p-sdgs__white-title">具体的な取り組み</p>
+                <ul class="p-sdgs__lists">
+                  <li class="p-sdgs__list">高い品質の工事（早く、より良く、安全に）</li>
+                  <li class="p-sdgs__list">安全パトロール</li>
+                  <li class="p-sdgs__list">品質・労働安全衛生マネジメントシステム（ISO9001・ISO45001）の推進</li>
+                </ul>
+              </div>
+            </div>
+            <figure class="p-sdgs__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/sdgs_1.png" alt="" width="250" height="250">
+            </figure>
+          </div>
+          <div class="p-sdgs__block p-sdgs__block--2">
+            <div class="p-sdgs__detail">
+              <p class="p-sdgs__detail-subTitle">地域共創</p>
+              <h3 class="p-sdgs__detail-title">地域の価値を高める</h3>
+              <p class="p-sdgs__text">地域の産業振興と豊かな環境を守り育て、持続可能性の向上に貢献していきます。</p>
+              <div class="p-sdgs__white">
+                <p class="p-sdgs__white-title">具体的な取り組み</p>
+                <ul class="p-sdgs__lists">
+                  <li class="p-sdgs__list">アグリ事業</li>
+                  <li class="p-sdgs__list">ソーラー発電事業</li>
+                  <li class="p-sdgs__list">地域へのボランティア活動</li>
+                  <li class="p-sdgs__list">環境マネジメントシステム（ISO14001）の推進</li>
+                </ul>
+              </div>
+            </div>
+            <figure class="p-sdgs__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/sdgs_2.png" alt="" width="250" height="250">
+            </figure>
+          </div>
+          <div class="p-sdgs__block p-sdgs__block--3">
+            <div class="p-sdgs__detail">
+              <p class="p-sdgs__detail-subTitle">地域共創</p>
+              <h3 class="p-sdgs__detail-title">社員のより良い暮らしの追及</h3>
+              <p class="p-sdgs__text">社員及び社員と生活を共にするご家族のためになる豊かな就労環境を整える。また、社員各々が社会に貢献できる能力開発にも努めます。</p>
+              <div class="p-sdgs__white">
+                <p class="p-sdgs__white-title">具体的な取り組み</p>
+                <div class="p-sdgs__lists-wrapper">
+                  <ul class="p-sdgs__lists">
+                    <li class="p-sdgs__list">WLBの充実化</li>
+                    <li class="p-sdgs__list">高齢者事故の防止対策</li>
+                    <li class="p-sdgs__list">社員教育環境の整備</li>
+                  </ul>
+                  <ul class="p-sdgs__lists">
+                    <li class="p-sdgs__list">産休育休介護休の拡充</li>
+                    <li class="p-sdgs__list">ハラスメント・ストレス対策の実施</li>
+                    <li class="p-sdgs__list">社内研修旅行の実施</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <figure class="p-sdgs__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/sdgs_3.png" alt="" width="250" height="250">
+            </figure>
+          </div>
+          <div class="p-sdgs__block p-sdgs__block--4">
+            <div class="p-sdgs__detail">
+              <p class="p-sdgs__detail-subTitle">地域共創</p>
+              <h3 class="p-sdgs__detail-title">パートナーの企業や人々も大切に</h3>
+              <p class="p-sdgs__text">ともに支え合う協力会社・地域の方々の持続可能性の確保に協力します。その責任を果たすためにも会社を持続させていきます。</p>
+              <div class="p-sdgs__white">
+                <p class="p-sdgs__white-title">具体的な取り組み</p>
+                <div class="p-sdgs__lists-wrapper">
+                  <ul class="p-sdgs__lists">
+                    <li class="p-sdgs__list">適切な取引関係</li>
+                    <li class="p-sdgs__list">事業継続計画（BCP）の運用</li>
+                    <li class="p-sdgs__list">寿文庫活動</li>
+                  </ul>
+                  <ul class="p-sdgs__lists">
+                    <li class="p-sdgs__list">コンプライアンス尊守の徹底</li>
+                    <li class="p-sdgs__list">安全協議会活動</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <figure class="p-sdgs__img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/sdgs_4.png" alt="" width="250" height="250">
+            </figure>
+          </div>
+        </div>
+       
       </div>
     </div>
   </section>
