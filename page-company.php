4730a997c5b9d22ede59a444e7535dd9e003cf63
yumauda <firststep-yumauda@gmail.com>
2025-09-18 11:45:27 +0900

    会社案内2割

---
+++ page-company.php
@@ -0,0 +1,82 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail">
+          <div class="p-page-mv__title">
+            <h1 class="c-main-title">
+              <span class="c-main-title__en">Company</span>
+              <span class="c-main-title__ja">会社案内</span>
+            </h1>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-page-mvImg">
+    <div class="l-inner">
+      <figure class="p-page-mvImg__img">
+        <img src="<?php echo get_template_directory_uri() ?>/images/common/mv_company.png" alt="会社案内" width="1700" height="500">
+      </figure>
+    </div>
+  </section>
+  <section class="p-message">
+    <div class="l-inner">
+      <div class="p-message__content">
+        <div class="p-message__detail">
+          <h2 class="p-message__title">地域社会の発展と豊かな郷土づくりを目指す。</h2>
+          <div class="p-message__text-wrapper">
+            <p class="p-message__text">1954年創業以来、65有余年にわたり皆様方の温かいご支援並びにご愛顧を賜り誠にありがとうございます。<br>
+              このたび、長岡秀治の後任として代表取締役社長に就任いたしました福田弘道でございます。<br>
+              社長の大役を仰せつかり、その責任の重さに身の引き締まる思いをいたしております。今後とも前任者同様皆様方のご支援とご協力を賜り、精一杯社業発展に邁進していく所存ですので何卒よろしくお願いいたします。<br>
+              顧みますと、父である創業者故福田寿夫が斐川の地に建設業としての礎を築き、そして長岡前社長において地域の建設業として確固たる地位を築いてまいりました。<br>
+              今後は、弊社の経営理念「製品ではなく商品を納める精神」、社訓「感謝・忍耐・貢献」の精神を受け継ぎ、尚一層の技術研鑽に努めるとともに、事業活動を通じて地域社会の発展と豊かな郷土づくりに貢献し、お客様から信頼される企業であり続けるために日々精進してまいりますので、今後ともご指導ご鞭撻を賜りますようお願い申し上げます。</p>
+          </div>
+          <div class="p-message__name-wrapper">
+            <p class="p-message__occupation">代表取締役社長</p>
+            <p class="p-message__name">福田 弘道</p>
+          </div>
+        </div>
+        <figure class="p-message__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/message_img.png" alt="福田 弘道" width="1002" height="731">
+        </figure>
+      </div>
+    </div>
+  </section>
+  <section class="p-philosophy">
+    <div class="l-inner">
+      <div class="p-philosophy__content">
+        <h2 class="p-philosophy__title">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/philosophy_logo.png" alt="論語" width="476" height="463">
+        </h2>
+        <div class="p-philosophy__block">
+          <div class="p-philosophy__text-wrapper">
+            <p class="p-philosophy__text">
+              論語に「君子は和して同せず。小人は同して和せず」とある。<br>
+              君子は可なれば賛同し、小人は不可でも阿り賛同する。<br>
+              君子は不可なれば賛同せず。<br>
+              是を真の「和」とするという意である。
+            </p>
+            <p class="p-philosophy__text">
+              我社では、お互いが和して敬いながらも、切磋琢磨をして自己研鑽に励むという意味を表しています。
+            </p>
+          </div>
+        </div>
+        <div class="p-philosophy__row">
+          <div class="p-philosophy__left">
+            <p class="p-philosophy__left-title">経営理念</p>
+            <p class="p-philosophy__left-text">製品ではなく商品を納める精神</p>
+          </div>
+          <div class="p-philosophy__right">
+            <p class="p-philosophy__right-title">社訓</p>
+            <p class="p-philosophy__right-text">感謝 忍耐 貢献</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+<?php get_footer() ?>
